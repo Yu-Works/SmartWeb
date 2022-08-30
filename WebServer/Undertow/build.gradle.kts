0e17c4afb55d23d246e608fd0a72b8cc029bf281
IceCream <www@withdata.net>
2022-08-30 21:15:39 +0800

    添加 Server: Undertow 项目

---
+++ WebServer/Undertow/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    kotlin("jvm")
+}
+
+dependencies{
+    api(rootProject)
+    api("io.undertow:undertow-core:2.2.18.Final")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,5 +4,9 @@ fun includeProject(name: String, dir: String? = null){
     include(name)
     dir?.let { project(name).projectDir = file(it) }
 }
+// HTTP Server
 includeProject(":SmartHTTP","WebServer/SmartHTTP")
+includeProject(":Undertow","WebServer/Undertow")
+
+// TempleEngine
 includeProject(":Rythm","TempleEngine/Rythm")
