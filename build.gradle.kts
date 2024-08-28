e4323d7a3efd5ef04dcd0a5aa22e8dbcca94c78d
icecream <www@withdata.net>
2024-08-29 00:27:53 +0800

    Github Action

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "2.0.10"
 }
 val coreVersion = "1.0.0-DEV1"
 version = "1.0.0-DEV1"
@@ -22,9 +22,18 @@ allprojects {
 
     pluginManager.apply(JavaLibraryPlugin::class.java)
     pluginManager.apply(MavenPublishPlugin::class.java)
+    apply {
+        plugin("org.jetbrains.kotlin.jvm")
+    }
 
     java {
         withSourcesJar()
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlin {
+        jvmToolchain(8)
     }
     configure<PublishingExtension> {
         publications {
@@ -67,10 +76,8 @@ allprojects {
 
 
                 credentials {
-
-                    val mvnInfo = readMavenUserInfo("IceCream")
-                    username = mvnInfo[0]
-                    password = mvnInfo[1]
+                    System.getenv("MAVEN_USER")?.let { username = it }
+                    System.getenv("MAVEN_TOKEN")?.let { password = it }
                 }
             }
         }
@@ -84,96 +91,4 @@ allprojects {
         implementation("commons-fileupload:commons-fileupload:1.5")
     }
 
-}
-
-dependencies {
-    implementation(kotlin("stdlib"))
-    api("org.ehcache:ehcache:3.10.1")
-}
-
-
-
-java {
-    withSourcesJar()
-}
-
-tasks {
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-        sourceCompatibility = "1.8"
-        targetCompatibility = "1.8"
-    }
-
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
-    }
-}
-
-//publishing {
-//
-//    publications {
-//        create<MavenPublication>(name) {
-//            groupId = group.toString()
-//            artifactId = name
-//            version = project.version.toString()
-//
-//            pom {
-//                name.set("Rain Java Dev Framework")
-//                description.set("Rain Java Dev Framework")
-//                url.set("https://github.com/IceCream-Open/Rain")
-//                licenses {
-//                    license {
-//                        name.set("The Apache License, Version 2.0")
-//                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-//                    }
-//                }
-//                developers {
-//                    developer {
-//                        id.set("IceCream")
-//                        name.set("IceCream")
-//                        email.set("www@withdata.net")
-//                    }
-//                }
-//                scm {
-//                    connection.set("")
-//                }
-//            }
-//            from(components["java"])
-//        }
-//    }
-//
-//    repositories {
-//        mavenLocal()
-//        maven {
-//            val snapshotsRepoUrl = "https://maven.icecreamqaq.com/repository/maven-snapshots/"
-//            val releasesRepoUrl = "https://maven.icecreamqaq.com/repository/maven-releases/"
-//            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-//
-//
-//            credentials {
-//
-//                val mvnInfo = readMavenUserInfo("IceCream")
-//                username = mvnInfo[0]
-//                password = mvnInfo[1]
-//            }
-//        }
-//    }
-//
-//}
-fun readMavenUserInfo(id: String) =
-    fileOr(
-        "mavenInfo.txt",
-        "${System.getProperty("user.home")}/.m2/mvnInfo-$id.txt"
-    )?.readText()?.split("|") ?: arrayListOf("", "")
-
-
-fun File.check() = if (this.exists()) this else null
-fun fileOr(vararg file: String): File? {
-    for (s in file) {
-        val f = file(s)
-        if (f.exists()) return f
-    }
-    return null
 }
